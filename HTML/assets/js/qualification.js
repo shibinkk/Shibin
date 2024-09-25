8e7a6d38c339c35fdbfee06c226b6da5fe7b7ffb
SHIBIN K K <shibinkk17@gmail.com>
2024-09-25 22:10:37 +0530

    edu

--- HTML/assets/js/qualification.js
+++ HTML/assets/js/qualification.js
@@ -1,20 +1,23 @@
-var $element=$('.each-event, .title');
+var $element = $('.each-event, .title');
 var $window = $(window);
+
 $window.on('scroll resize', check_for_fade);
 $window.trigger('scroll');
-function check_for_fade() { 
+
+function check_for_fade() {
     var window_height = $window.height();
-    
-    $.each($element, function (event) {
+    var scroll_top = $window.scrollTop();
+
+    $.each($element, function () {
         var $element = $(this);
         var element_height = $element.outerHeight();
         var element_offset = $element.offset().top;
-        space = window_height - (element_height + element_offset -$(window).scrollTop());
-        if (space < 60) {
-            $element.addClass("non-focus");
+
+        // Check if the element is within the viewport
+        if (scroll_top + window_height > element_offset + 50) {
+            $element.addClass("fade-in");
         } else {
-            $element.removeClass("non-focus");
+            $element.removeClass("fade-in");
         }
- 
     });
-};
+}
